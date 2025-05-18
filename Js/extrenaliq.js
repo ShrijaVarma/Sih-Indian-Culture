8ff0bb5356c79879df51920f58f990e550c6f90d
Shrija Varma <shrijavarma2713@gmail.com>
2025-05-18 15:18:45 +0530

    Add typewriter

--- Js/extrenaliq.js
+++ Js/extrenaliq.js
@@ -5,3 +5,13 @@ $(window).scroll(function () {
         $('nav').removeClass('nav-fixed');
     }
 });
+//typingtext
+var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
