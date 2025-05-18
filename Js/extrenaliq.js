9398d1fd04aca99116afc68c65a5aeafdbf6545a
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-18 17:56:35 +0530

    updated extrenaljq.js

--- Js/extrenaliq.js
+++ Js/extrenaliq.js
@@ -15,3 +15,21 @@ var TxtType = function (el, toRotate, period) {
     this.tick();
     this.isDeleting = false;
 };
+TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) {
+        delta /= 2;
+    }
